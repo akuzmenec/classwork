b8b8c177b04e1c80b14fb4b0204605f26642e6ce
akuzmenec <akuzmenec@yandex.ru>
2026-05-30 21:06:42 +0300

    task_1

---
+++ main.py
@@ -0,0 +1,36 @@
+# 1
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+sum = a + b + c
+prod = a * b * c
+
+print(f"sum = {sum}, product = {prod}")
+
+#2
+
+sal = int(input("salary:"))
+cred = int(input("credit:"))
+ar = int(input("arrears:"))
+
+result = sal - cred - ar
+
+print(result)
+
+#3
+d_1 = int(input())
+d_2 = int(input())
+
+s = (d_1 + d_2)/2
+
+print(s)
+
+#4
+
+print("To be \nor not \nto be")
+
+#4
+
+print("Life is what happenes \n when \n  you're buse making other plans")
