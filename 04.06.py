55768df4489c237b9d17c559b0f6024888b4c315
Взрослая академия <georgygevorkov@gmail.com>
2026-06-04 19:27:35 +0300

    3 num

--- 04.06.py
+++ 04.06.py
@@ -3,24 +3,16 @@
 #1
 num1 = int(input())
 num2 = int(input())
+num3 = int(input())
 
 if num1 > num2:
-    print(num1)
-elif num1 == num2:
-    print(f"{num1} = {num2}")
-else:
+    if num1 > num3:
+        print(num1)
+    else:
+        print(num2)
+
+elif num2 > num1:
     print(num2)
 
-
-#2
-s = int(input())
-t = int(input())
-print("middle speed:", s/t)
-
-
-#3
-num3 = int(input())
-a = num3 % 10
-b = (num3//10) % 10
-c = num3//100
-print(f"{a}{b}{c}")
+else:
+    print(num3)
