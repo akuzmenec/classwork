c04f1467a3ca4c4ef26f0e0259d93c9fa3455110
akuzmenec <akuzmenec@yandex.ru>
2026-07-02 21:09:16 +0300

    function

---
+++ 02.07.py
@@ -0,0 +1,92 @@
+from random import randint
+# ls = [0,0,0,0,0,0,0,0,0,0]
+# a = int(input())
+# b = int(input())
+# for i in range(len(ls)):
+#     ls[i] = randint(a, b)
+# print(ls)
+
+# #2
+# max = ls[0]
+# ind = 0
+# for j in range(len(ls)):
+#     if ls[j] > max:
+#         max = ls[j]
+#         ind = j
+# print(f"{max}, ind:{ind}")
+#
+# #3
+#
+# ls2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# n = 0
+# for i in range(len(ls2)):
+#     if ls[i] % 2 == 0:
+#         ls2[n] = ls[i]
+#         n += 1
+#
+# for i in range(len(ls2)):
+#     if ls[i] % 2 != 0:
+#         ls2[n] = ls[i]
+#         n += 1
+#
+#
+# print(ls2)
+# #
+# def start_pr():
+#     print("-начало программы-")
+#
+# start_pr()
+# print("n1")
+#
+#
+# def max_ls(ls):
+#     max = ls[0]
+#     for i in range(len(ls)):
+#         if ls[i] > max:
+#             max = ls[i]
+#     return max
+# ls1 = [1,2,3,4,5]
+#
+# print(max_ls(ls1))
+#
+#
+# def random_ls(ls):
+#     for i in range(len(ls)):
+#         ls[i] = randint(1, 100)
+#     return ls
+# ls1 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# print(random_ls(ls1))
+
+# def av(ls):
+#     sum = 0
+#     for i in ls:
+#         sum += 1
+#     return sum/len(ls)
+#
+# ls1 = [[1,2,3,4,5],[3,2,4,5,5], [4,5,3,2,1]]
+#
+#
+# def max_ls(ls):
+#     max = ls[0]
+#     for i in range(len(ls)):
+#         if ls[i] > max:
+#             max = ls[i]
+#     return max
+# ls1 = [[1,2,3,4,5], [3,2,4,6,5], [4,5,3,2,1]]
+#
+# max1 = 0
+# index_max = 0
+# for i in range(len(ls1)):
+#     if max_ls(ls1[i]) > max1:
+#         max1 = max_ls(ls1[i])
+#         index_max = i
+# print(index_max)
+
+
+
+
+
+
+
+
+
